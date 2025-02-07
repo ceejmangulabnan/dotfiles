252e2cf603be2bc4f8c90c7dc445f9c93b46538d
Cheljee Mangulabnan <cheljee.mangulabnan@gmail.com>
2025-02-08 05:05:30 +0800

    feat: commit before update ml4w

---
+++ nvim/.config/nvim/lua/custom/plugins/timetracker.lua
@@ -0,0 +1,18 @@
+return {
+  '3rd/time-tracker.nvim',
+  dependencies = {
+    '3rd/sqlite.nvim',
+  },
+  event = 'VeryLazy',
+  opts = {
+    data_file = vim.fn.stdpath 'data' .. '/time-tracker.db',
+  },
+
+  config = function()
+    require('time-tracker').setup {
+      data_file = vim.fn.stdpath 'data' .. '/time-tracker.db',
+      tracking_events = { 'BufEnter', 'BufWinEnter', 'CursorMoved', 'CursorMovedI', 'WinScrolled' },
+      tracking_timeout_seconds = 5 * 60, -- 5 minutes
+    }
+  end,
+}
