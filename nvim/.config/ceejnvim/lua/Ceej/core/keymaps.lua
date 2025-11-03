2575d75462a8890c8e7f32b87cd2f892686a97f0
Ceej Mangulabnan <cheljee.mangulabnan@gmail.com>
2025-11-03 11:05:08 +0800

    feat(ceejnvim): add general keymaps and options

---
+++ nvim/.config/ceejnvim/lua/Ceej/core/keymaps.lua
@@ -0,0 +1,37 @@
+-- ----------- General Keymaps -----------
+
+vim.keymap.set('n', '<C-d>', '<C-d>zz', { desc = 'Scroll down half page' })
+vim.keymap.set('n', '<C-u>', '<C-u>zz', { desc = 'Scroll up half page' })
+vim.keymap.set('n', '<C-Up>', '<Cmd>resize -2<CR>')
+vim.keymap.set('n', '<C-Down>', '<Cmd>resize +2<CR>')
+vim.keymap.set('n', '<C-Left>', '<Cmd>vertical resize -2<CR>')
+vim.keymap.set('n', '<C-Right>', '<Cmd>vertical resize +2<CR>')
+
+vim.keymap.set('n', ';', ':', { desc = 'Command' })
+vim.keymap.set('v', ';', ':', { desc = 'Command in v' })
+
+vim.keymap.set('n', ',p', '"0p', { desc = 'Paste yanked' })
+vim.keymap.set('v', ',p', '"0p', { desc = 'Paste yanked' })
+
+vim.keymap.set('n', '<leader>cl', '<cmd>%s/\r//g<CR>', { desc = 'Clear' })
+vim.keymap.set('n', '<leader>tn', '<cmd>tabnew<CR>', { desc = 'New Tab' })
+vim.keymap.set('n', '<C-[>', '<cmd>tabprevious<CR>', { desc = 'Go to Previous Tab' })
+vim.keymap.set('n', '<C-]>', '<cmd>tabnext<CR>', { desc = 'Go to Next Tab' })
+
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+
+-- Window/Pane Navigation
+vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
+vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
+vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+
+
+
+-- ----------- Plugin Keymaps ----------- 
+
+-- Oil
+vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+
+-- Telescope
+
---
+++ nvim/.config/ceejnvim/lua/Ceej/core/options.lua
@@ -0,0 +1,15 @@
+vim.o.number = true
+vim.o.relativenumber = true
+vim.o.mouse = 'a'
+vim.o.clipboard = 'unnamedplus'
+vim.o.breakindent = true
+vim.o.undofile = true
+vim.o.cursorline = true
+vim.o.hlsearch = true
+vim.o.list = true
+vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+vim.o.smartindent = true
+vim.o.tabstop = 2
+vim.o.softtabstop = 2
+vim.o.shiftwidth = 2
+vim.o.expandtab = true
